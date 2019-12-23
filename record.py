Record capability now extends to the Spectra Sensor.  Added averaged recording time to the dataset as well.  One bug that I just encountered was the reading starts over at 1 now when leaving the program, will need to fix this.  Next steps are to clean up the program to make more visually aestetic and clean up some of the functionality.  Program seems to be mostly functional though and I should be able to attempt to integrate with all the systems.

record.py
@@ -31,6 +31,7 @@ class Recording:
         self.time_end = self.time_start+self.record_length
         self.avg_on = True                  # turn on averaging
         self.rdg = init_reading_number      # initialized reading number
+        self.incr = False                   # initialize incrementer
         self._check_for_latest_reading()     # pull in reading from existing file (if exists)
 
         self.iheaders = ['Reading','DateTime']  # start with initial headers
@@ -42,8 +43,11 @@ class Recording:
             self.filename = self.filename[0:self.filename.find('.csv')]
 
         # If fileLoc doesn't end with \ then append it
-        if self.fileLoc[-1] != '\\':
-            self.fileLoc = self.fileLoc + '\\'
+        try:
+            if self.fileLoc[-1] != '\\':
+                self.fileLoc = self.fileLoc + '\\'
+        except:
+            self.fileLoc = os.getcwd() + '\\'
 
         # Finally, combine all necessary elements of filename into full_filename
         self.full_filename = self.fileLoc + self.filename + '.csv'
@@ -54,7 +58,59 @@ class Recording:
             self.rdg = rdg_from_file+1
 
     def captureDataSS(self, values):
-        pass
+        '''Captures data for the spectra sensor.'''
+        # Copy the values so that we don't modify the original list
+        vals = values.copy()
+
+        # Spectra Sensor Data ------------------------------------------
+        # Indicate if recording is enabled and if this is a continuation
+        if self.recEnabled and time.time()<=self.time_end:
+            # Insert reading num and time into dataset
+            vals.insert(0,datetime.datetime.now())
+            vals.insert(0,self.rdg)
+
+            if self.continuationSS:
+                pass   # This is a continuation so we must append the data to the list
+
+            else:
+                # This is the first point to record, so we must create the new variable to store data
+                self.dataSS = []   # reinitialize to clear data from previous recording
+
+                # Turn ON continuation mode
+                self.continuationSS = True
+
+            self.dataSS.append(vals)
+
+
+        else:   #  we are not in record mode
+            if self.continuationSS:  # we just came out of a record
+                self.continuationSS = False   # Force out of continuation
+
+                # Go to interpret output function
+                try:
+                    self.interpretOutput(self.dataSS, 'WVSS')
+
+                    # Notate info about recording
+                    print('WVSS Recording Captured')
+                    print('WVSS Data Length = {} rows in {} seconds'.format(len(self.dataSS), time.time()-self.time_start))
+
+                    # Get ready for next recording
+                    self.incr = True  # increment reading number
+
+                except:  # file is open cannot write so throw an error
+                    msg = common_def.error_msg()
+                    msg.setText('DATA NOT WRITTEN TO FILE:\n\n{} is open; '.format(self.filename_ext) + \
+                                'please close the file before continuing')
+                    msg.exec_()
+
+            self.continuationSS = False  # turn off continuation
+
+        # Use here only because this is the last to be run during a recording
+        if self.incr:
+            self.rdg += 1   # increment reading number
+            self.incr = False
+
+
 
     def captureDataLC(self, values):
         '''Captures data for the humidity generator.'''
@@ -87,14 +143,14 @@ class Recording:
 
                 # Go to interpret output function
                 try:
-                    self.interpretOutput()
+                    self.interpretOutput(self.dataLC, 'HumGen')
 
                     # Notate info about recording
-                    print('Recording Captured')
-                    print('Data Length = {} rows in {} seconds'.format(len(self.dataLC), time.time()-self.time_start))
+                    print('HumGen Recording Captured')
+                    print('HumGen Data Length = {} rows in {} seconds'.format(len(self.dataLC), time.time()-self.time_start))
 
                     # Get ready for next recording
-                    self.rdg += 1  # increment reading number
+                    self.incr = True  # increment reading number
 
                 except:  # file is open cannot write so throw an error
                     msg = common_def.error_msg()
@@ -105,10 +161,10 @@ class Recording:
 
             self.continuationLC = False  # turn off continuation
 
-    def interpretOutput(self):
+    def interpretOutput(self, data, src):
         '''After reading has been captured, this method evaluates what to do with it.'''
         # Convert to dataframe
-        out_df = self.convertToDataFrame(self.dataLC)
+        out_df = self.convertToDataFrame(data, src)
 
         # If a file has already been created append data to it, otherwise create
         #  the file and write data to it
@@ -117,7 +173,7 @@ class Recording:
         else:   # file does NOT exist, so create it and write the first point with header
             out_df.to_csv(self.full_filename, mode='w', index=False, header=True)
 
-    def convertToDataFrame(self, dataArray):
+    def convertToDataFrame(self, dataArray, src):
         # Create initial dataset from dataLC and dataSS
         df = pd.DataFrame(dataArray, columns=self.headers)
 
@@ -126,12 +182,16 @@ class Recording:
             # Work with time first
             most_recent = df.DateTime.max()   # Take max date for use in averaged data
             date = most_recent.strftime('%Y-%m-%d')
-            time = most_recent.strftime('%H:%M:%S.%f')
+            Time = most_recent.strftime('%H:%M:%S.%f')
+
+            print('time_start = {}, type = {}'.format(self.time_start,type(self.time_start)))
+            rec_time = time.time() - self.time_start
 
             # Prep to combine averaged data
             row_name = 'mean'
             df.loc[row_name] = df.mean(axis=0)   # Average all columns in the last row of the dataframe
-            ini_cols = pd.DataFrame({'Reading': [self.rdg], 'Date': [date], 'Time': [time]}, index=[row_name])  # Initial (fixed) columns
+            ini_cols = pd.DataFrame({'Reading': [self.rdg], 'Source': [src], 'Seconds Avg': [rec_time],\
+                           'Date': [date], 'Time': [Time]}, index=[row_name])  # Initial (fixed) columns
             sel_cols = df.iloc[[-1],2:]   # Last row and Columns that were selected by the user
             out_df = pd.concat([ini_cols,sel_cols], axis=1, sort=False)
         else: